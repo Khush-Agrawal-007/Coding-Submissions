c57b164318c4efd70d43574d12b33f63045c1a94
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-01-26 13:23:36 +0530

    Time: 77 ms (60.45%), Space: 19.1 MB (73.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,20 +4,14 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if not prices:
-            return 0
-        
-        min_price = float('inf')  # Initialize to a very high value
-        max_profit = 0            # Initialize max profit to 0
-        
+        min_price = float('inf')
+        max_profit = 0
+
         for price in prices:
-            # Update the minimum price seen so far
             if price < min_price:
                 min_price = price
-            
-            # Calculate the profit if we sold at the current price
-            # and update max_profit if this profit is higher
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-                
+            else:
+                max_profit = max(max_profit, price - min_price)
+
         return max_profit
+                
