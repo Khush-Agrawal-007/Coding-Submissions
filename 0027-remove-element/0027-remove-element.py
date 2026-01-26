897fea51bfb7d9e38bdd4a7af56be5678116e2c8
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-01-27 00:19:37 +0530

    Time: 0 ms (100%), Space: 12.6 MB (14.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,18 +5,53 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        # 'k' serves as the writer pointer. 
-        # It marks the index where the next valid element should go.
-        k = 0
-        
-        # 'i' serves as the reader pointer.
+        k = 0 
         for i in range(len(nums)):
-            # If the current element is NOT the value we want to remove...
-            if nums[i] != val:
-                # ...we write it to the 'k' position
-                nums[k] = nums[i]
-                # ...and move the writer forward
-                k += 1
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # 'k' serves as the writer pointer. 
+        # # It marks the index where the next valid element should go.
+        # k = 0
+        
+        # # 'i' serves as the reader pointer.
+        # for i in range(len(nums)):
+        #     # If the current element is NOT the value we want to remove...
+        #     if nums[i] != val:
+        #         # ...we write it to the 'k' position
+        #         nums[k] = nums[i]
+        #         # ...and move the writer forward
+        #         k += 1
                 
-        # Return the number of valid elements
-        return k
+        # # Return the number of valid elements
+        # return k
