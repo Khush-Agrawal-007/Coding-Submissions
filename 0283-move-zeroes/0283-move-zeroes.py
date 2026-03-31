beed0c3ee5c7a6dcc27b9c87ed45748b1ed377c8
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-03-31 14:59:25 +0530

    Time: 6 ms (62.23%), Space: 13.7 MB (10.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,39 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        n = len(nums)
-        while i < n:
-            if nums[i] == 0:
-                nums.pop(i)
-                nums.append(0)
-                n = n-1
-            else:
-                i = i+1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # for i in range(len(nums)):
+        # i = 0
+        # n = len(nums)
+        # while i < n:
         #     if nums[i] == 0:
         #         nums.pop(i)
         #         nums.append(0)
-        # return nums
+        #         n = n-1
+        #     else:
+        #         i = i+1
 
-
-        
+        ## TWO POINTER APPROACH 
+        left = 0
+        for right in range(len(nums)):
+            if nums[right]!=0 :
+                nums[left],nums[right] = nums[right],nums[left]
+                left +=1     
