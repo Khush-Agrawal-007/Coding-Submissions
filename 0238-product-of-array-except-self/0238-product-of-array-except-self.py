23a1ce22c5ae5f0d454a03a4cac5212d3e29dd83
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-04-04 13:16:10 +0530

    Time: 49 ms (18.29%), Space: 23.5 MB (9.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,14 +5,17 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        answer = [1] * n
-        prefix = 1
+
+        left = [1] * n
+        right = [1] * n
+        ans = [1] * n 
+
+        for i in range(1,n):
+            left[i] = left[i-1] * nums[i-1]
+        
+        for i in range(n-2,-1,-1):
+            right[i] = right[i+1] * nums[i+1]
+
         for i in range(n):
-            answer[i] = prefix
-            prefix *= nums[i]
-        suffix = 1
-        for i in range(n - 1, -1, -1):
-            answer[i] *= suffix
-            suffix *= nums[i]
-            
-        return answer
+            ans[i] = left[i] * right[i]
+        return ans
