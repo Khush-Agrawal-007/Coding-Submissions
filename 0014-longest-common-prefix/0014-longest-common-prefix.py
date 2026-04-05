ddf4b3117ea604aa1657953eca2a0932d13fa422
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-04-05 15:59:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (81.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,16 +1,27 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        if not strs:
-            return "null"
+        # if not strs:
+        #     return "null"
 
-        prefix = strs[0]
-        for word in strs[1:]:
-            j = 0
-            while j < len(prefix) and j < len(word) and prefix[j] == word[j]:
-                j += 1
-            prefix = prefix[:j]
-            if not prefix:
-                return ""
-        return prefix
+        # prefix = strs[0]
+        # for word in strs[1:]:
+        #     j = 0
+        #     while j < len(prefix) and j < len(word) and prefix[j] == word[j]:
+        #         j += 1
+        #     prefix = prefix[:j]
+        #     if not prefix:
+        #         return ""
+        # return prefix
             
-        
+
+
+        # sort then see 1st and last string only and find common from both only
+        if not strs:
+            return ""
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        i = 0
+        while i< len(first)and i <len(last) and first[i] == last[i]:
+            i+=1
+        return first[:i]
