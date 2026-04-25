bc382b37dca3a2587acb1c675b3cc2184cd7de15
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-04-25 18:50:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (55.35%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,9 +1,10 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        s = s.strip()
-        return len(s[s.rfind(' ')+1:])
-        
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        s=s.strip()
+        i=len(s)-1
+        while i>=0 and s[i]!=" ":
+            count+=1
+            i-=1
+        return count
+
