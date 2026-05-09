a41eb797b51589486cb39704a62810e57c6a69e2
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-05-09 18:00:35 +0530

    Time: 15 ms (42.91%), Space: 20.4 MB (8.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,33 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s.lower()
-        t.lower()
-        if len(s)!=len(t):
+        # s.lower()
+        # t.lower()
+        # if len(s)!=len(t):
+        #     return False
+        # freq = [0]*26
+        # s = s.replace(' ','')
+        # t = t.replace(' ','')
+        # for ch in s:
+        #     freq[ord(ch)-ord('a')] +=1
+        # for ch in t:
+        #     freq[ord(ch)-ord('a')] -=1
+        # for i in freq:
+        #     if i!=0:
+        #         return False
+        # return True
+        
+        if len(s) != len(t):
             return False
-        freq = [0]*26
-        s = s.replace(' ','')
-        t = t.replace(' ','')
-        for ch in s:
-            freq[ord(ch)-ord('a')] +=1
-        for ch in t:
-            freq[ord(ch)-ord('a')] -=1
-        for i in freq:
-            if i!=0:
-                return False
-        return True
+        
+        # tset = set(t)
+
+        # for char in tset:
+        #     if s.count(char) != t.count(char):
+        #         return False
+        # return True 
+
+        s=sorted(s)
+        t = sorted(t)
+        if s == t:
+            return True
+        return False
