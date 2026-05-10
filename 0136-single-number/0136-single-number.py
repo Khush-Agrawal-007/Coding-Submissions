5ccb10424cd0fd2912d21028215779ad47b709a3
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-05-10 13:49:35 +0530

    Time: 19 ms (18.61%), Space: 14.6 MB (15.95%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,19 @@
+from collections import Counter
 class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+
         ## BASIC IMPLEMENTATION OF XOR GATE  ' ^ '
-        result = 0
-        for i in nums:
-            result = i ^ result
-        return result
+        # result = 0
+        # for i in nums:
+        #     result = i ^ result
+        # return result
+
+        count = Counter(nums)
+
+        for num in count:
+            if count[num] ==1:
+                return num
