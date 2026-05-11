40656602ce7a5aea2a9e1845b3d5113212855b47
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-05-11 14:04:36 +0530

    Time: 0 ms (100%), Space: 12.8 MB (26.83%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -4,8 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # res = []
+        # for i in nums:
+        #     for digit in str(i):
+        #         res.append(int(digit))
+        # return res
+
         res = []
         for i in nums:
-            for digit in str(i):
-                res.append(int(digit))
+            temp = []
+            while i > 0:
+                temp.append(i%10)
+                i//=10
+
+            res.extend(temp[::-1])
         return res
